e1f779f3ca9d4406461a25c679ae059cce705454
farkas.sebestyen <farkas.sebestyen@ssm.hu>
2025-03-05 09:15:16 +0100

    logo src

--- next-quiz/src/app/[user]/Header.tsx
+++ next-quiz/src/app/[user]/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import logo from "../../../public/logoWhite.png";
 import { notFound, usePathname } from "next/navigation";
 import Link from "next/link";
 import LanguageSelector from "./LanguageSelector";
@@ -22,7 +21,13 @@ const Header = ({ username }: { username: string | undefined }) => {
     return (
         <div className={styles.headerWrapper}>
             <div className={styles.logoCt}>
-                <Image src={logo} alt="DRV logo" className={styles.logo} height={50} width={50}/>
+                <Image 
+                    src={"/logoWhite.png"} 
+                    alt="DRV logo" 
+                    className={styles.logo} 
+                    height={50} 
+                    width={50} 
+                />
             </div>
             <div className={styles.headerMessageCt}></div>
             <div className={styles.navCt}>
