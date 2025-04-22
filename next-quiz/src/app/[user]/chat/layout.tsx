38f1248d6add774494dab1015e54e68b98a3e3b1
Naram99 <131804895+Naram99@users.noreply.github.com>
2025-04-22 23:00:23 +0200

    chat layout and page added

---
+++ next-quiz/src/app/[user]/chat/layout.tsx
@@ -0,0 +1,8 @@
+export default function ChatLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <h1>Chat Layout</h1>
+            <div>{children}</div>
+        </div>
+    );
+}
