42ce41e5fe442aa5a8dfaa6f6442d2b395b5eb89
Naram99 <131804895+Naram99@users.noreply.github.com>
2025-04-24 20:40:09 +0200

    socket dc

--- socket-server/src/index.ts
+++ socket-server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +9,10 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-io.on("connection", (socket) => {})
+io.on("connection", (socket) => {
+    socket.on("disconnect", (username) => {
+        console.log(`${username} disconnected.`);
+    });
+});
 
 httpServer.listen(process.env.PORT || 3210);
-
