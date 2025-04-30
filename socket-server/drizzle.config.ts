a26c5cc6d6d0788f0ceeecdb20047d5a8f853d04
Naram99 <farkassebi@gmail.com>
2025-04-30 19:54:39 +0200

    drizzle.config.ts added to socket server

---
+++ socket-server/drizzle.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from "drizzle-kit";
+
+export default defineConfig({
+    out: "./src/drizzle/migrations",
+    schema: "./src/drizzle/schema.ts",
+    strict: true,
+    verbose: true,
+    dialect: "postgresql",
+    dbCredentials: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME || "",
+        host: process.env.DB_HOST || "127.0.0.1",
+        port: Number(process.env.DB_PORT) || 5432,
+        ssl: false,
+    }
+})
