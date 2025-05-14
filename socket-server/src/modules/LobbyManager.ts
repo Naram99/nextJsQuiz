b97c05be26ae2b8dd7ed85a8bb87f144e363c965
Naram99 <farkassebi@gmail.com>
2025-05-14 22:18:02 +0200

    lobby create

--- socket-server/src/modules/LobbyManager.ts
+++ socket-server/src/modules/LobbyManager.ts
@@ -2,4 +2,13 @@ import Lobby from "./Lobby";
 
 export default class LobbyManager {
     private lobbies: Map<string, Lobby> = new Map();
+
+    /**
+     * createLobby
+     */
+    public createLobby(code: string): Lobby {
+        const lobby = new Lobby(code);
+        this.lobbies.set(code, lobby);
+        return lobby;
+    }
 }
