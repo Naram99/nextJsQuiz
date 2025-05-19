ede793f7eb0c6ca95522ed5dae72f16b4c811de3
Naram99 <farkassebi@gmail.com>
2025-05-19 22:29:47 +0200

    lobby page

--- next-quiz/src/app/lobby/[id]/page.tsx
+++ next-quiz/src/app/lobby/[id]/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { connectSocketWithFreshToken, socket } from "@/socket/socket";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function LobbyPage() {
     const params = useParams();
     const id = params.id;
 
     const [users, setUsers] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (!socket.connected) {
+            connectSocketWithFreshToken();
+        }
+
+        socket.emit("getLobbyData", id);
+
+        // TODO: socket.on("lobbyData")
+    });
+
+    // TODO: lobby page
 }
